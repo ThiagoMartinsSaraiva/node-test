9488be9208937527706b9c246b738d6af5914315
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2020-02-09 01:39:30 -0300

    adding some tests

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -1,10 +1,28 @@
 const calcModule = require('../calc')
 const { expect } = require('chai')
 
-describe('calc test', function () {
-  it('sum should return 4', function (done) {
+describe('calc test', () => {
+  it('sum should return 4', (done) => {
     const result = calcModule.sum(2, 2)
     expect(result).to.equal(4)
+    done()
+  })
+
+  it('sub should return 0', done => {
+    const result = calcModule.sub(2, 1)
+    expect(result).to.equal(1)
+    done()
+  })
+
+  it('mult should return 6', done => {
+    const result = calcModule.mult(3, 2)
+    expect(result).to.equal(6)
+    done()
+  })
+
+  it('div should return 2', done => {
+    const result = calcModule.div(2, 1)
+    expect(result).to.equal(2)
     done()
   })
 })
